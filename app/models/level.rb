474ff1288301394784d53cda630cac655639efa9
akio6706 <campin616@gamil.com>
2021-02-24 18:41:22 +0900

    active hashの導入

---
+++ app/models/level.rb
@@ -0,0 +1,11 @@
+class Level < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'レベル1' },
+    { id: 3, name: 'レベル2' },
+    { id: 4, name: 'レベル3' }
+  ]
+  
+  include ActiveHash::Associations
+  has_many :recipes
+end
