66374e1e957a63ccb840917344486c3d5a7752d1
akio6706 <campin616@gamil.com>
2021-02-25 18:59:27 +0900

    レシピ投稿をデータベースに保存

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,6 +1,6 @@
 class Level < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
+    { id: 1, name: '▽ ▽ ▽' },
     { id: 2, name: 'レベル1' },
     { id: 3, name: 'レベル2' },
     { id: 4, name: 'レベル3' }
--- app/models/recipe_ingredient_procedure.rb
+++ app/models/recipe_ingredient_procedure.rb
@@ -8,16 +8,15 @@ class RecipeIngredientProcedure
     validates :description
     validates :people
     validates :user_id
-    validates :recipe_id
     validates :ingredient
     validates :amount
     validates :procedure
   end
   validates :level_id, numericality: { other_than: 1 }
   def save
-    recipe = Recipe.create(image: image, title: title, description: description, people: people, level_id: level_id, user_id: user_id)
-    Ingredient.create(ingredient: ingredient, amount: amount, recipe_id: recipe.id)
-    Procedure.create(procedure: procedure, recipe_id: recipe.id)
+    Recipe.create(image: image, title: title, description: description, people: people, level_id: level_id, user_id: user_id)
+    Ingredient.create(ingredient: ingredient, amount: amount, recipe_id: recipe_id)
+    Procedure.create(procedure: procedure, recipe_id: recipe_id)
   end
 end
 
