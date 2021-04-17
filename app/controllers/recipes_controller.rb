bf487262e18a28393c91983f4826b723723ee9c2
akio6706 <campin616@gamil.com>
2021-04-17 22:14:06 +0900

    コメントビューファイル作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -22,6 +22,8 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find(params[:id])
+    @comment = Comment.new
+    @comments = @recipe.comments.includes(:user)
   end
 
   def edit
