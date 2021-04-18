5839bb84e362a5fd58debdc94ae5c424ce5855fd
akio6706 <campin616@gamil.com>
2021-04-18 18:16:39 +0900

    usersコントローラー,route実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :recipes do
     resources :comments, only: :create
   end
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
