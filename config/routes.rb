847e5123b410fbe663f0b618ea55d717c337ab77
akio6706 <campin616@gamil.com>
2021-04-21 21:43:49 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   root to: 'recipes#index'
   resources :recipes do
     resources :comments, only: :create
+    collection do
+      get 'level'
+    end
   end
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: :show
 end
